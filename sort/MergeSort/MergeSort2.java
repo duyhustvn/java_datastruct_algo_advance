9a0da6ce11fcb3e815927e146927f735f62bd974
duyleomessi <duyleitbka95@gmail.com>
2018-07-31 10:20:48 +0700

    add data

--- sort/MergeSort/MergeSort2.java
+++ sort/MergeSort/MergeSort2.java
@@ -1,4 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class MergeSort2 {
+
+    public int[] readFile(File file) {
+        Scanner sc;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e){
+            System.out.println("File not found");
+            return new int[]{-1};
+        }
+        
+        // int arCount = Integer.parseInt(sc.nextLine().trim());
+
+        String[] arrStr = sc.nextLine().split(" ");
+        int[] arr = new int[arrStr.length];
+        for (int i = 0; i < arrStr.length; i++) {
+            int arrItem = Integer.parseInt(arrStr[i].trim());
+            arr[i] = arrItem;
+        }
+        sc.close();
+        return arr;
+    }
+
     public void mergeSort(int[] arr) {
         int length = arr.length;
         if (length > 1) {
@@ -30,7 +57,7 @@ public class MergeSort2 {
                 i++;
                 k++;
             } else {
-                mergeArr[k] = arrL[j];
+                mergeArr[k] = arrR[j];
                 j++;
                 k++;
             }
@@ -49,10 +76,13 @@ public class MergeSort2 {
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[] { 1, 3, 4, 3, 5, 23, 1, 232 };
+
         MergeSort2 mergeSort = new MergeSort2();
 
+        File file = new File("data.txt");
+        int[] arr = mergeSort.readFile(file);
         mergeSort.mergeSort(arr);
         mergeSort.printArray(arr);
+
     }
 }
