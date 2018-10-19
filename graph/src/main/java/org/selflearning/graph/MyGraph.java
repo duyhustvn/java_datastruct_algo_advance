44a8170a0c170c98f771553986b07e864c42d085
duyleomessi <duyleitbka95@gmail.com>
2018-10-19 18:02:12 +0700

    implement bfs

--- graph/src/main/java/org/selflearning/graph/MyGraph.java
+++ graph/src/main/java/org/selflearning/graph/MyGraph.java
@@ -1,5 +1,5 @@
 package org.selflearning.graph;
-import java.util.List;
+import java.util.*;
 
 public abstract class MyGraph {
     private int numVertices;
@@ -27,6 +27,41 @@ public abstract class MyGraph {
         implementAddEdge(f, t);
         this.numEdges++;
     }
+
+    /* Algorithm
+    Next node: Queue
+    Visited set: HashSet
+    parent: HashMap
+
+    Add start to Queue and visited set
+    while Queue is not empty 
+        dequeu start from queue as current node
+        if current == end return parent map
+        foreach current neighbor node, n, not in visited n
+            add n to visted set
+            add current node as n's parent in parent map
+            enqueue n to queue
+    */
+    public HashMap<Integer, Integer> BFS( int start, int end) {
+        Queue<Integer> nextNode = new LinkedList<Integer>();
+        HashSet<Integer> visited = new HashSet<Integer>();
+        HashMap<Integer, Integer> parent = new HashMap<Integer, Integer>();
+        nextNode.add(start);
+        visited.add(start);
+        Integer current;
+        while (nextNode.size() != 0) {
+            current = nextNode.remove();
+            if (current == end) {
+                return parent;
+            }
+            List<Integer> neighbors = this.getOutDegree(current);
+            for (Integer neighbor: neighbors) {
+                visited.add(neighbor);
+                parent.put(current, neighbor);
+                nextNode.add(neighbor);
+            }
+        }
+    }   
 
     public abstract void implementAddVertex();
     public abstract void implementAddEdge(int f, int t);
